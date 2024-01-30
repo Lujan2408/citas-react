995abb85e03bd25c7ce1e486f71c07ed41039497
Jose David <lujan9495@gmail.com>
2024-01-30 12:44:43 -0500

    creando botones para editar y eliminar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,6 +12,14 @@ const Formulario = ({ pacientes, setPacientes }) => {
 
   const [error, setError] = useState(false)
 
+  const generarId = () => { 
+    const random = Math.random().toString(36).substring(2)
+    const dateNow = Date.now().toString(36)
+    
+    return random + dateNow
+
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault(); 
     
@@ -30,7 +38,8 @@ const Formulario = ({ pacientes, setPacientes }) => {
       propietario, 
       email, 
       fecha, 
-      sintomas
+      sintomas,
+      id: generarId()
     }
 
     setPacientes([...pacientes, objetoPaciente])
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,28 +1,39 @@
 /* eslint-disable react/prop-types */
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({ pacientes }) => {
-  console.log(pacientes)
+const ListadoPacientes = ({pacientes}) => { 
 
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
 
+      { pacientes && pacientes.length ? 
+      
+      <>
         <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {''}
+              <span className="font-bold text-indigo-600">Pacientes y Citas</span>
+          </p>
 
-        <p className="text-xl mt-5 mb-10 text-center">
-          Administra tus {''}
-            <span className="font-bold text-indigo-600">Pacientes y Citas</span>
-        </p>
+          { pacientes.map( paciente => (
+            <Paciente 
+              paciente={paciente}
+              key={paciente.id}
+            />
+          ))}
+      </>
+      
+      : 
+      <>
+        <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Agrega tus pacientes {''}
+                <span className="font-bold text-indigo-600">y Administralos</span>
+            </p>
+      </>
+      }  
 
-        { pacientes.map( paciente => (
-          <Paciente
-            paciente={paciente}
-            key={paciente}
-          >
-          </Paciente>
-        ))}    
-
-        <Paciente />
+        
 
     </div>
   )
